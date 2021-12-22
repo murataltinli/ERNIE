31b273b3b05ebc844bd1d297fd37a22f3bcd6111
murataltinli <95572169+murataltinli@users.noreply.github.com>
2021-12-22 18:07:11 +0300

    Add files via upload

--- include/Positron_Angle.h
+++ include/Positron_Angle.h
@@ -1,16 +1,8 @@
-/*
-  Vogel, P., and J. F. Beacom. “Angular Distribution of Neutron Inverse Beta Decay,
-   ν ¯ e + p → e + + n.” Physical Review D 60, no. 5 (July 27, 1999): 053003.
-    https://doi.org/10.1103/PhysRevD.60.053003.
-*/
-
 #ifndef Positron_Angle
 #define Positron_Angle
 
 #include <random>
 #include <cmath>
-
-#include "MeanCosTheta.h"
 
 using namespace std;
 
@@ -33,7 +25,7 @@ double positron_dist(double E_nu, double costheta_e)
   double v_e1 = p_e1 / E_e1;
   double G_f = 1.1663787 * pow(10,-11); // Fermi coupling constant
   double costc = 0.974; // cosinus of cabbibo angle
-  double Delta_R_inner = 0.024;
+  double Delta_R_inner = 0.024; // nucleus independant radiative corrections
   double sigma0 = (pow(G_f,2) * pow(costc,2) / M_PI) * (1 +  Delta_R_inner);
   double Gamma = 2 * (f + f2) * g * ((2 * E_e + Delta) * (1 - v_e * costheta_e) - (pow(m_e,2) / E_e))
                 + (pow(f,2) + pow(g,2)) * (Delta * (1 + v_e * costheta_e) + (pow(m_e,2) / E_e))
@@ -55,33 +47,16 @@ double positron_Angle(double E_nu, int seed = 1)
   while(true)
   { 
     double x = d1(gen);
-    
-    if(meanCosTheta(E_nu) < 0)
+    double y = d2(gen) * (positron_dist(E_nu,-1));   
+    if(y <= positron_dist(E_nu,x))
     {
-      double y = d2(gen) * (positron_dist(E_nu,-1));   
-      if(y <= positron_dist(E_nu,x))
-      {
-        return x;
-        break;
-      }
-      else
-      {
-        continue;
-      }  
+      return x;
+      break;
     }
     else
     {
-      double y = d2(gen) * (positron_dist(E_nu,1));
-      if(y <= positron_dist(E_nu,x))
-      {
-        return x;
-        break;
-      }
-      else
-      {
-        continue;
-      }  
-    }
+      continue;
+    }      
   } 
 }
 
