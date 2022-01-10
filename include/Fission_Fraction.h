7442c7e256bab65cc2692efda42a87f1bd0f7b72
murataltinli <95572169+murataltinli@users.noreply.github.com>
2022-01-10 19:33:06 +0300

    Add files via upload

---
+++ include/Fission_Fraction.h
@@ -0,0 +1,16 @@
+#ifndef Fission_Fraction
+#define Fission_Fraction
+
+struct FissionFraction 
+{
+  double U235_i;
+  double U238_i;
+  double Pu239_i;
+  double Pu241_i;
+  double U235_f;
+  double U238_f;
+  double Pu239_f;
+  double Pu241_f;
+};
+
+#endif
