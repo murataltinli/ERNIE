e2a022b4b1bcec9db77fd04e39b6a9e62fad600a
murataltinli <95572169+murataltinli@users.noreply.github.com>
2022-01-11 18:51:28 +0300

    Add files via upload

--- include/Positron_Angle.hh
+++ include/Positron_Angle.hh
@@ -1,8 +1,10 @@
 #ifndef Positron_Angle
 #define Positron_Angle
 
+#include <random>
+
 double positron_dist(double, double);
 
-double positron_Angle(double, int);
+double positron_Angle(double, std::default_random_engine&);
 
 #endif
