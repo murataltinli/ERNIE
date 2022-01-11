ea255bee705dd59cb7871a8e8531d57085d826cd
murataltinli <95572169+murataltinli@users.noreply.github.com>
2022-01-11 17:33:41 +0300

    Update Energy_Momentum.hh

--- include/Energy_Momentum.hh
+++ include/Energy_Momentum.hh
@@ -4,7 +4,7 @@
 #define M_p 938.2720813 //proton mass (MeV/c^2)
 #define M_n 939.5654133 //neutron mass (MeV/c^2)
 #define m_e 0.511 // positron mass (MeV/c^2)
-#define M ((M_n - M_p) / 2) // average nucleon mass
+#define M ((M_n + M_p) / 2) // average nucleon mass
 #define Delta (M_n - M_p)
 
 double positron_Energy(double, double);
