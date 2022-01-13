739674c3d7b1bba30c191c2e046cc39049480448
murataltinli <95572169+murataltinli@users.noreply.github.com>
2022-01-13 18:09:18 +0300

    Add files via upload

--- include/Energy_Momentum.hh
+++ include/Energy_Momentum.hh
@@ -1,11 +1,12 @@
 #ifndef Energy_Momentum
 #define Energy_Momentum
 
-#define M_p 938.2720813 //proton mass (MeV/c^2)
-#define M_n 939.5654133 //neutron mass (MeV/c^2)
-#define m_e 0.511 // positron mass (MeV/c^2)
-#define M ((M_n + M_p) / 2) // average nucleon mass
-#define Delta (M_n - M_p)
+const double M_p = 938.2720813; //proton mass (MeV/c^2)
+const double M_n = 939.5654133; //neutron mass (MeV/c^2)
+const double m_e = 0.511; // positron mass (MeV/c^2)
+const double M = ((M_n + M_p) / 2); // average nucleon mass
+const double Delta = (M_n - M_p);
+const double y2 = ((Delta*Delta) - (m_e*m_e)) / 2;
 
 double positron_Energy(double, double);
 
